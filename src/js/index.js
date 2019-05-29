feeb6b3c987064aae3f84dfff7a0468b99c6dc9b
Frankie Bukenya <frankie.bukenya@gmail.com>
2019-05-29 06:48:07 +0300

    Loop Over all the check boxes

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,27 @@
 
 const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
 
+// Reference to the last checked item
+let lastChecked;
+
+// EventListener to know which check box was checked
 function handleCheck(e) {
-    console.log(e);
+  // Check to see that they have the shift key down
+  // See if the input is being checked
+  let inBetween = false;
+  if (e.shiftKey && this.checked) {
+    // Go ahead and what we please
+    // Loop over every check box
+    checkboxes.forEach(checkbox => {
+      console.log(checkbox);
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+        console.log('Starting to check inbetween the checkboxes');
+      }
+    });
+  }
+
+  lastChecked = this;
 }
 
 checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
